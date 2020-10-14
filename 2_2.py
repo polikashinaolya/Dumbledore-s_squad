4ca977844acef1e7ee87a2efc1b4f3ac5348e516
polikashinaolya <72521921+polikashinaolya@users.noreply.github.com>
2020-10-14 22:04:42 +0300

    Create 2_2.py

---
+++ 2_2.py
@@ -0,0 +1,36 @@
+import json5
+
+def text_analysis(text, maxlen, forbidden_words):
+    result = {}
+    result.update([('length', len(text))])
+
+    result.update([('pure_length',
+                    len(''.join(text.split()))
+                        )])
+
+    result.update([('origin_text', text)])
+
+    pure_text = []
+    for word in text.split():
+        if word in forbidden_words:
+            pure_text.append('***')
+        else:
+            pure_text.append(word)
+    result.update([('pure_text', ' '.join(pure_text))])
+
+    pure_short_text = ''
+    if len(text) > maxlen:
+        for i in range(maxlen):
+            pure_short_text += text[i]
+        pure_short_text += '...'
+    else:
+        pure_short_text = text
+    result.update([('pure_short_text', pure_short_text)])
+
+    with open('result.json', 'w') as json:
+        json5.dump(result, json)
+
+text_i = input()
+maxlen_i = int(input())
+forbidden_words_i = input().split()
+text_analysis(text_i, maxlen_i, forbidden_words_i)
